5a642d8154059f2b257c600b65b68842b8a37d7e
rohanyadav05 <yadav.rohan@thinkfuture.us>
2021-09-16 00:20:31 +0530

    intitial

--- variables&types/variables1.go
+++ variables&types/variables1.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+//global vs local varibles
+
+//defining global variables
+var Fname="Rohan"
+
 func  main()  {
 	fmt.Println("Git setup")
+	Lname:="Yadav"
+	fmt.Println(Fname,"+",Lname)
+
 }
